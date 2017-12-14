98b1a8a81c50813e2a97389356ea12058a03532e
psnider30 <pbsnider@gmail.com>
2017-12-13 17:52:48 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-// Code goes here
+import { message } from './components/foo';
+
+// In a file in the same directory
+import fruit from './fruits';
+console.log(fruit.apple); // prints 'red'
+
+// In another file, also in the same directory
+import { apple } from './fruits';
+console.log(apple); // prints 'red'
+
+import Tweet from './Tweet';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <Tweet />,
+  document.getElementById('root')
+);
